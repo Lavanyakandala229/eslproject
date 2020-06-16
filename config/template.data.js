d1e7f39814c7caadbc7f439ac53e7fe4d4d6bb95
LavanyaKandala229 <lavanyakandala9@gmail.com>
2020-06-16 08:46:07 +0530

    first Commit

---
+++ config/template.data.js
@@ -0,0 +1,13 @@
+"use strict";
+/**
+ * 
+ * This file is a set of general data that will be used by the Templates,
+ * you can add any data to this file and it will be added to the task runner.
+ *  
+ */
+
+module.exports = {
+
+    "title": "elc-coding-test-master-aede79cc272cea41a964edefb1387f125bfca040"
+
+};
